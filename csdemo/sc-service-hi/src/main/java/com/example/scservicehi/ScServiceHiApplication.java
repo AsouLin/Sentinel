d0fe43da81e66a9242b62c31a96a5d449d5332f7
AsouLin <linjunluo2001@163.com>
2023-07-27 00:32:02 +0800

    Fix version and check demo

--- csdemo/sc-service-hi/src/main/java/com/example/scservicehi/ScServiceHiApplication.java
+++ csdemo/sc-service-hi/src/main/java/com/example/scservicehi/ScServiceHiApplication.java
@@ -1,24 +1,21 @@
 package com.example.scservicehi;
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.ServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 @RestController
 public class ScServiceHiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run( ScServiceHiApplication.class, args );
+        SpringApplication.run(ScServiceHiApplication.class, args);
     }
 
     @Value("${server.port}")
